ea83e4a3473350bcf0bb34a3623dd16e3117bbf9
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2019-03-12 22:00:15 +0100

    fix: lint

--- list.py
+++ list.py
@@ -5,7 +5,7 @@ from sys import argv
 from boardgamegeek import BGGClient, BGGClientLegacy
 from boardgamegeek.cache import CacheBackendSqlite
 
-list_id = argv[1] if len(argv)>1 else '253162'
+list_id = argv[1] if len(argv) > 1 else '253162'
 
 cache1 = CacheBackendSqlite(path=".cache.bgg1", ttl=3600*24)
 bgg1 = BGGClientLegacy(cache=cache1)
@@ -14,13 +14,16 @@ print(f"[{list.id}] {list.name}\n{list.description}")
 
 cache2 = CacheBackendSqlite(path=".cache.bgg2", ttl=3600*24)
 bgg2 = BGGClient(cache=cache2)
-games_id_list = [item.object.id for item in list
-                 if item.object.type == 'thing' and item.object.subtype == 'boardgame']
+games_id_list = [
+        item.object.id for item in list
+        if item.object.type == 'thing' and item.object.subtype == 'boardgame'
+    ]
 games = bgg2.game_list(games_id_list)
 games_dict = {game.id: game for game in games}
 
 for item in list:
-    print(f"  [{item.object.id}] img:{item.object.imageid} {item.object.name}", end='')
+    print(f"  [{item.object.id}]"
+          f" img:{item.object.imageid} {item.object.name}", end='')
     game = games_dict[item.object.id]
     print(f" #{game.bgg_rank}", end='')
     print(f" year:{game.year}", end='')
